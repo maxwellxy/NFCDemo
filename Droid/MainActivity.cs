1aaefcc333a242d06ab92d1215b92e244f2e3b55
Shan <11812r@gmail.com>
2016-08-30 11:37:10 +0530

    Initial Commit

---
+++ Droid/MainActivity.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Nfc;
+using Poz1.NFCForms.Droid;
+using Poz1.NFCForms.Abstract;
+using Android.Media;
+
+namespace NFCDemo.Droid
+{
+	[Activity (Label = "NFCDemo.Droid", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+
+	//[MetaData (NfcAdapter.ActionNdefDiscovered, Resource="@xml/nfc")]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+	{
+		
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			global::Xamarin.Forms.Forms.Init (this, bundle);
+
+
+			Xamarin.Forms.DependencyService.Register<INfcForms, NfcForms>();
+
+			LoadApplication (new App ());
+
+			//BroadcastRingerReceiver RingerReceiver = new BroadcastRingerReceiver ();
+			//
+			//IntentFilter RingerFilter1 = new IntentFilter (NfcAdapter.ActionTagDiscovered);
+			//IntentFilter RingerFilter2 = new IntentFilter (NfcAdapter.ActionNdefDiscovered);
+			//IntentFilter RingerFilter3 = new IntentFilter (NfcAdapter.ActionTechDiscovered);
+			//
+			//RegisterReceiver (RingerReceiver, RingerFilter1);
+			//RegisterReceiver (RingerReceiver, RingerFilter2);
+			//RegisterReceiver (RingerReceiver, RingerFilter3);
+		}
+	}
+	/*
+	public class BroadcastRingerReceiver : BroadcastReceiver
+	{
+		public override void OnReceive (Context context, Intent intent)
+		{
+			
+		}
+	}
+	*/
+
+}
+
---
+++ Droid/SecondActivity.cs
@@ -0,0 +1,55 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Nfc;
+using Poz1.NFCForms.Droid;
+using Poz1.NFCForms.Abstract;
+using Android.Media;
+
+namespace NFCDemo.Droid
+{
+	[Activity (Label = "SecondActivity")]		
+	[IntentFilter (new[]{AudioManager.RingerModeChangedAction,NfcAdapter.ActionTagDiscovered,NfcAdapter.ActionNdefDiscovered,NfcAdapter.ActionTechDiscovered},Categories = new[]{Intent.CategoryDefault})]
+	public class SecondActivity : Activity
+	{
+		public NfcAdapter NFCdevice;
+		public NfcForms x;
+		protected override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+			NfcManager NfcManager = (NfcManager)Android.App.Application.Context.GetSystemService(Context.NfcService);
+			NFCdevice = NfcManager.DefaultAdapter;
+
+			x = Xamarin.Forms.DependencyService.Get<INfcForms>() as NfcForms;
+			// Create your application here
+
+			Intent CurrentIntent = this.Intent;
+			Bundle bundle = CurrentIntent.Extras;
+
+			foreach (string key in bundle.KeySet()) 
+			{
+				Object value = bundle.Get(key);
+				Console.WriteLine("Key: "+key+" , value: "+value.ToString() +" , value class: "+value.GetType().Name);
+			}
+		}
+
+
+
+		protected override void OnPause()
+		{
+			base.OnPause();
+			NFCdevice.DisableForegroundDispatch(this);
+		}
+
+	}
+}
+
